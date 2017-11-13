0a0b467023a544e864026f0dc93bf2d93e93de94
Ben French <ben.jminw@gmail.com>
2017-11-13 15:44:22 -0700

    Initial HTML and js.

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+var hangmanGame = {
+    wordList: ["Scarlet Begonias", "Slipknot", "China Cat Sunflower", "St Stephen", "Tennessee Jed", "Dark Star"],
+    currentWord: '',
+    lettersGuessedAlready: [],
+    guessesRemaining: 25,
+    wins: 0,
+
+    // Update the DOM with new values. This will be called at the end of most
+    // of the other functions.
+    updateDOM: function() {
+
+    },
+    
+    initWord: function(word) {
+
+    },
+
+    // Guess a letter and update the data
+    guessLetter: function(letter) {
+
+    },
+
+    // Check if the user has won
+    checkForWin: function() {
+
+    }
+};
